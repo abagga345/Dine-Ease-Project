ae71719a83db9c2d708a9fc2e54ae3f3598f78c8
AAYUSH BAGGA <aayushbagga2005@gmail.com>
2024-07-12 03:48:40 +0530

    ADDED STYLING TO SIGNUP COMPONENT,RESUSABILITY ADDITION STILL LEFT

--- frontend/DineEase/src/App.tsx
+++ frontend/DineEase/src/App.tsx
@@ -1,14 +1,36 @@
-// import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {UserSignup} from "./Components/User/UserSignup"
+import {UserSignin} from "./Components/User/UserSignin"
+import {UserHome} from "./Components/User/UserHome"
+import {UserCart} from "./Components/User/UserCart"
+import {UserProfile} from "./Components/User/UserProfile"
+import {UserMenu} from "./Components/User/UserMenu"
+import { AdminSignin } from "./Components/Admin/AdminSignin"
+import {AdminSignup} from "./Components/Admin/AdminSignup"
+import {AdminDashboard} from "./Components/Admin/AdminDashboard"
 import './App.css'
 
 function App() {
- 
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserSignup></UserSignup>}></Route>
+          <Route path="/signin" element={<UserSignin></UserSignin>}></Route>
+          <Route path="/home" element={<UserHome></UserHome>}></Route>
+          <Route path="/profile" element={<UserProfile></UserProfile>}></Route>
+          <Route path="/menu" element={<UserMenu></UserMenu>}></Route>
+          <Route path="/cart" element={<UserCart></UserCart>}></Route>
+          <Route path="/admin/signup" element={<AdminSignup></AdminSignup>}></Route>
+          <Route path="/admin/signin" element={<AdminSignin></AdminSignin>}></Route>
+          <Route path="/admin/dashboard" element={<AdminDashboard></AdminDashboard>}></Route>
+        </Routes>
+      </BrowserRouter>
      
     </>
   )
 }
 
 export default App
+
+
---
+++ frontend/DineEase/src/Components/User/UserSignup.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+import { SignupCard } from "./SignupCard"
+export function UserSignup(){
+    return (
+        <div className="h-screen">
+            <SignupCard></SignupCard>
+        </div>
+        
+    )
+}
+// fields={} onSubmitUrl={} navigateto={} message={}
