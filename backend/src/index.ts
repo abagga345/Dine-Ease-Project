ac7f6951cb751166f6e835c9e6c2b7ebe9f8b793
AAYUSH BAGGA <aayushbagga2005@gmail.com>
2025-05-22 00:04:29 +0530

    Port changes for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use("/api/v1",mainRouter);
 
 
-app.listen(3000,function(){
-    console.log("SERVER STARTED ON PORT 3000"); 
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
